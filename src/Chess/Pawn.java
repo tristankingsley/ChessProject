7fa45af02b4866bc2898451873d08d1701870e05
tristankingsley <46898839+tristankingsley@users.noreply.github.com>
2019-02-26 10:21:24 -0500

    First Class

---
+++ src/Chess/Pawn.java
@@ -0,0 +1,20 @@
+package Chess;
+
+public class Pawn extends ChessPiece {
+
+	public Pawn(Player player) {
+		super(player);
+	}
+
+	public String type() {
+		return "Pawn";
+	}
+
+	// determines if the move is valid for a pawn piece
+	public boolean isValidMove(Move move, IChessPiece[][] board) {
+
+		boolean valid = true;
+        // More code is needed
+		return valid;
+	}
+}
