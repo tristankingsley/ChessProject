743abe9764f0c9e7fde9e43038102d60fa11bc4b
Trevor S <trevorspitzley@gmail.com>
2019-03-09 17:46:10 -0500

    Added a small snip of my idea for checking the path for the bishop. It isn't the cleanest but it works currently for moving the bishop up/left direction. Not that many lines, so once i clean it up it would just be like four chunks of statements for the bishop to not let it skip. Let me know what you think.

--- src/Chess/Bishop.java
+++ src/Chess/Bishop.java
@@ -13,11 +13,29 @@ public class Bishop extends ChessPiece {
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
 
 		if(super.isValidMove(move, board)) {
-			if (Math.abs(move.fromRow - move.toRow)
-					== Math.abs(move.fromColumn - move.toColumn) &&
-					(move.fromColumn != move.toColumn ||
-							move.fromRow != move.toRow))
+			if (clearPath(move, board)) {
+				if (Math.abs(move.fromRow - move.toRow)
+						== Math.abs(move.fromColumn - move.toColumn) &&
+						(move.fromColumn != move.toColumn ||
+								move.fromRow != move.toRow))
+					return true;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean clearPath(Move move, IChessPiece[][] board){
+		//int to represent spots in between piece before the selected spot
+		int numHops = ( (Math.abs(move.toColumn - move.fromColumn) ) - 1);
+
+		//If statement for moving upward/leftward
+		if (move.toColumn < move.fromColumn &&
+				move.toRow < move.fromRow){
+			//If statement using numHops to look at spots
+			if (board[move.toRow + numHops][move.toColumn + numHops] == null){
 				return true;
+			}
 		}
 
 		return false;
