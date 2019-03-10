e292aa88cfdd5d5ef3cf4f833891c216b97cd075
Trevor S <trevorspitzley@gmail.com>
2019-03-10 13:31:56 -0400

    Let me know what ya think

--- src/Chess/Rook.java
+++ src/Chess/Rook.java
@@ -22,12 +22,58 @@ public class Rook extends ChessPiece {
 					|| Math.abs(move.fromRow - move.toRow) == 0) &&
 					(move.fromColumn != move.toColumn ||
 							move.fromRow != move.toRow)
-			))
+			) && clearPath(move, board))
 				return true;
 		}
 
 		return false;
 		
+	}
+
+	public boolean clearPath(Move move, IChessPiece[][] board){
+		//numHops calculates # of hops required. Regardless of direction.
+		int numHops = ( ((Math.abs(move.fromRow - move.toRow) * 1) -1) +
+				((Math.abs(move.fromColumn - move.toColumn) * 1) -1) );
+
+		//Set to 1, 0 for default upward movement.
+		int addX = 0;
+		int addY = -1;
+
+
+		//changes if the move is downward
+		if(move.fromRow < move.toRow) {
+			addY = 1;
+		}
+
+		//changes if movement is rightward
+		if(move.fromColumn < move.toColumn){
+			addX = 1;
+			addY = 0;
+		}
+		//covers leftward movement
+		if (move.fromColumn > move.toColumn) {
+			addX = -1;
+			addY = 0;
+		}
+
+		//sets the coordinates of the first cell to be tested
+		int initRow = move.fromRow + addY;
+		int initCol = move.fromColumn + addX;
+
+		//runs until the number of places moved is reached
+		for(int i = 0; i < numHops; i++) {
+			if (board[initRow][initCol] != null)
+				return false;
+			else {
+
+				//incremented values based on above
+				initRow += addY;
+				initCol += addX;
+			}
+		}
+
+		return true;
+
 	}
 	
 }
